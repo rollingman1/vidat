6e35e6db836f8fe4126c90e8c4b032fd68ada8a0
rollingman1 <bloodtype0@khu.ac.kr>
2023-05-26 16:09:45 +0900

    feat: 경로 수정

--- tools/backend/index.js
+++ tools/backend/index.js
@@ -35,22 +35,22 @@ function get_videos(base)
     videos = {}
 
     // read video directory
-    fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'video')).forEach(name_folder => {
-        fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'video', name_folder)).forEach(file => {
+    fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'video')).forEach(name_folder => {
+        fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'video', name_folder)).forEach(file => {
         ext = file.split('.').pop()
         if ((ext == 'avi') || (ext == 'mp4')) {
-            video_root = path.join('230524_action_confirm_poc', 'video', name_folder)
+            video_root = path.join('230525_action_confirm_poc', 'video', name_folder)
             name = path.join(name_folder, file.substr(0, file.length - ext.length - 1))
             videos[name] = {video: path.join(video_root,file), annotation: null, confirmAnnotation: null}
         }
     })})
 
     // read annotation directory
-    fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'annotation_notConfirmed')).forEach(name_folder => {
-        fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'annotation_notConfirmed', name_folder)).forEach(file => {
+    fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'annotation_notConfirmed')).forEach(name_folder => {
+        fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'annotation_notConfirmed', name_folder)).forEach(file => {
         ext = file.split('.').pop()
         if (ext == 'json') {
-            anno_root = path.join('230524_action_confirm_poc', 'annotation_notConfirmed', name_folder)
+            anno_root = path.join('230525_action_confirm_poc', 'annotation_notConfirmed', name_folder)
             name = path.join(name_folder, file.substr(0, file.length - ext.length - 1))
             if (name in videos) {
                 videos[name].annotation = path.join(anno_root,file)
@@ -59,11 +59,11 @@ function get_videos(base)
     })})
 
     // read confirmed annotation directory
-    fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'annotation_confirmed')).forEach(name_folder => {
-        fs.readdirSync(path.join(base, '230524_action_confirm_poc', 'annotation_confirmed', name_folder)).forEach(file => {
+    fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'annotation_confirmed')).forEach(name_folder => {
+        fs.readdirSync(path.join(base, '230525_action_confirm_poc', 'annotation_confirmed', name_folder)).forEach(file => {
         ext = file.split('.').pop()
         if (ext == 'json') {
-            anno_root = path.join('230524_action_confirm_poc', 'annotation_confirmed', name_folder)
+            anno_root = path.join('230525_action_confirm_poc', 'annotation_confirmed', name_folder)
             name = path.join(name_folder, file.substr(0, file.length - ext.length - 1))
             if (name in videos) {
                 videos[name].confirmAnnotation = path.join(anno_root, file)
@@ -90,7 +90,7 @@ app.post('/', (req, res) => {
         return res.status(500).send('Invalid token')
     }
     
-    const base = path.join(path.join(__dirname, 'vidat'), '230524_action_confirm_poc', 'annotation_confirmed')
+    const base = path.join(path.join(__dirname, 'vidat'), '230525_action_confirm_poc', 'annotation_confirmed')
     fs.writeFile(path.join(base, name + '.json'), json, (err, data) => {
         if (err) {
             console.log(err)
